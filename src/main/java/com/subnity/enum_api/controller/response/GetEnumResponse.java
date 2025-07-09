b2df80e03afafe6b03076aa25dd2040614b79703
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-07-09 15:21:29 +0900

    [FEAT] Enum 응답 객체 생성 (#16)

---
+++ src/main/java/com/subnity/enum_api/controller/response/GetEnumResponse.java
@@ -0,0 +1,21 @@
+package com.subnity.enum_api.controller.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "특정 Enum 조회 응답 객체")
+public class GetEnumResponse {
+
+  @Schema(description = "enum 코드")
+  private String code;
+
+  @Schema(description = "enum 값")
+  private String value;
+}
