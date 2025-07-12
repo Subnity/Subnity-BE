8740f96ef1e4b6d550055f7b3700d83c28ffd57f
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-07-12 20:49:03 +0900

    [FEAT] 토큰 재발급 로직 구현 (#18)

--- src/main/java/com/subnity/security/service/AuthService.java
+++ src/main/java/com/subnity/security/service/AuthService.java
@@ -1,8 +1,38 @@
 package com.subnity.security.service;
 
+import com.subnity.domain.member.enums.Role;
+import com.subnity.security.dto.JwtBuilder;
+import com.subnity.security.dto.JwtClaimsDto;
+import com.subnity.security.utils.JwtUtils;
+import com.subnity.security.utils.RedisUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+  public String recreateAccessToken(HttpServletRequest request) {
+    String authHeader = request.getHeader("Authorization");
+
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      String token = authHeader.split("Bearer ")[1];
+      String refreshToken = RedisUtils.get(token);
+
+      if (refreshToken != null && JwtUtils.getValidateToken(refreshToken)) {
+        String memberId = JwtUtils.getMemberId(refreshToken);
+        String role = JwtUtils.getRole(refreshToken);
+
+        JwtBuilder jwtDto = JwtUtils.createToken(
+          JwtClaimsDto.builder()
+            .memberId(memberId)
+            .role(Role.valueOf(role))
+            .build()
+        );
+
+        return jwtDto.getAccessToken();
+      }
+    }
+
+    return null;
+  }
 }
