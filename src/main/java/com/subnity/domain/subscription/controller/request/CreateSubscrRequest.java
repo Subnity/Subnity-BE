7eea1f567a7916a1b8f97de7a3fbd3cf53a5f22f
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-07-15 20:25:32 +0900

    [FEAT] 구독 생성 요청 객체 구현 (#26)

---
+++ src/main/java/com/subnity/domain/subscription/controller/request/CreateSubscrRequest.java
@@ -0,0 +1,45 @@
+package com.subnity.domain.subscription.controller.request;
+
+import com.subnity.common.utils.enums.SubscrCategory;
+import com.subnity.common.utils.enums.SubscrStatus;
+import com.subnity.domain.subscription.enums.PaymentCycle;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+
+@Schema(description = "구독 생성 요청 객체")
+public record CreateSubscrRequest(
+
+  @Schema(description = "구독 ID")
+  String subscriptionId,
+
+  @Schema(description = "플랫폼 이름")
+  String platformName,
+
+  @Schema(description = "설명")
+  String description,
+
+  @Schema(description = "요금")
+  String cost,
+
+  @Schema(description = "결제 주기")
+  PaymentCycle paymentCycle,
+
+  @Schema(description = "구독 상태")
+  SubscrStatus status,
+
+  @Schema(description = "구독 카테고리")
+  SubscrCategory category,
+
+  @Schema(description = "알림 여부")
+  Boolean isNotification,
+
+  @Schema(description = "해지일")
+  LocalDate cancelledAt,
+
+  @Schema(description = "결제일")
+  LocalDate lastPaymentDate,
+
+  @Schema(description = "다음 결제일")
+  LocalDate nextPaymentDate
+) { }
