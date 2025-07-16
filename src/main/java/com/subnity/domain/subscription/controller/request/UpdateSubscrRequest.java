36cb6b7d932b7209a136dbe6c59e929a48a56854
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-07-16 12:32:21 +0900

    [CHORE] 구독 정보 수정 요청 객체 구현 (#26)

---
+++ src/main/java/com/subnity/domain/subscription/controller/request/UpdateSubscrRequest.java
@@ -0,0 +1,23 @@
+package com.subnity.domain.subscription.controller.request;
+
+import com.subnity.common.utils.enums.SubscrCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "구독 정보 수정 요청 객체")
+public record UpdateSubscrRequest(
+
+  @Schema(description = "플랫폼 이름")
+  String platformName,
+
+  @Schema(description = "설명")
+  String description,
+
+  @Schema(description = "요금")
+  String cost,
+
+  @Schema(description = "카테고리")
+  SubscrCategory category,
+
+  @Schema(description = "알림 여부")
+  Boolean isNotification
+) { }
