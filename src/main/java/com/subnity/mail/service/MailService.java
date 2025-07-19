88f388ea126864249b83cf0c3ec19ada8ec6ca81
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-07-19 15:06:45 +0900

    [CHORE] openfeign 의존성 추가 (#34)

---
+++ src/main/java/com/subnity/mail/service/MailService.java
@@ -0,0 +1,14 @@
+package com.subnity.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * MailService : 메일 관련 Service
+ */
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+
+}
